6feacab674a909b15089a365b19d7fb0d73c2e4d
lkafedzic1 <zivotinje20.12.1997>
2020-12-27 14:27:51 +0100

    ok

---
+++ server-static.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public")); 
+
+app.get('/aktivnost', (req,res) => {
+    res.sendFile('./server-static/aktivnost.html', { root: __dirname});
+});
+
+app.get('/planiranjeNastavnik', (req,res) => {
+    res.sendFile('./server-static/planiranjeNastavnik.html', { root: __dirname});
+});
+
+app.get('/podaciStudent', (req,res) => {
+    res.sendFile('./podaciStudent.html', { root: __dirname});
+});
+
+app.get('/raspored', (req,res) => {
+    res.sendFile('./server-static/raspored.html', { root: __dirname});
+});
+
+app.get('/spirala2rasporedi', (req,res) => {
+    res.sendFile('./server-static/spirala2rasporedi.html', { root: __dirname});
+});
+
+app.get('/test', (req,res) => {
+    res.sendFile('./server-static/test.html', { root: __dirname});
+});
+
+//app.listen(3000);
+app.listen(port, () => console.log(`listening on port ${port}!`));
