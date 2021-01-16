5e13ac09b0bf96337496964ef6eaae02e19c5014
lkafedzic1 <zivotinje20.12.1997>
2021-01-16 08:53:21 +0100

    okej

--- db.js
+++ db.js
@@ -14,7 +14,45 @@ const db = {};
 db.Sequelize = Sequelize;  
 db.sequelize = sequelize;
 
-//import modela
+//importi modela
+// db.aktivnost = sequelize.import(__dirname+'/aktivnost.js');
+// db.dan = sequelize.import(__dirname+'/dan.js');
+// db.grupa = sequelize.import(__dirname+'/grupa.js');
+// db.predmet = sequelize.import(__dirname+'/predmet.js');
+// db.student = sequelize.import(__dirname+'/student.js');
+// db.tip = sequelize.import(__dirname+'/tip.js');
+
+db.aktivnost = require(__dirname+'/aktivnost.js')(sequelize, Sequelize);
+db.dan = require(__dirname+'/dan.js')(sequelize, Sequelize);
+db.grupa = require(__dirname+'/grupa.js')(sequelize, Sequelize);
+db.predmet = require(__dirname+'/predmet.js')(sequelize, Sequelize);
+db.student = require(__dirname+'/student.js')(sequelize, Sequelize);
+db.tip = require(__dirname+'/tip.js')(sequelize, Sequelize);
+
 //relacije
+//Predmet 1-N Grupa
+db.predmet.hasMany(db.grupa,{foreignKey:{allowNull:false}});
+db.grupa.belongsTo(db.predmet);
+
+// Aktivnost ​ N-1​ Predmet
+db.predmet.hasMany(db.aktivnost,{foreignKey:{allowNull:false}});
+db.aktivnost.belongsTo(db.predmet);
+
+// Aktivnost ​ N-0​ Grupa
+db.grupa.hasMany(db.aktivnost);
+db.aktivnost.belongsTo(db.grupa);
+
+// Aktivnost ​ N-1​ Dan
+db.dan.hasMany(db.aktivnost,{foreignKey:{allowNull:false}});
+db.aktivnost.belongsTo(db.dan);
+
+// Aktivnost ​ N-1​ Tip
+db.dan.hasMany(db.aktivnost,{foreignKey:{allowNull:false}});
+db.aktivnost.belongsTo(db.dan);
+
+// Student ​ N-M​ Grupa
+db.studentGrupa = db.grupa.belongsToMany(db.student,{as:'studenti',through:'student_grupa',foreignKey:'grupaId'});
+db.student.belongsToMany(db.grupa,{as:'grupe',through:'student_grupa',foreignKey:'studentId'});
+
 
 module.exports = db;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+const db = require('./db.js');
+
+// force: true will drop the table if it already exists
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync with { force: true }');
+});
+
+// require('./crud_routes.js')(app);
+
+// Create a Server
+var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log(`Example app listening at http://localhost:${ port }`)
+})
+
+
